5c8aaebe35c3ec15423f1ca64390a3bc3ef7c0aa
Carlos Iriarte <ciriarte@pivotal.io>
2020-02-14 16:09:28 -0800

    fix: it looks like I need to use test before a condition
    Authored-by: Carlos Iriarte <ciriarte@pivotal.io>

--- hack/update-gcp-dns.sh
+++ hack/update-gcp-dns.sh
@@ -64,7 +64,7 @@ function with_backoff {
 function check_resolved_ip {
   resolved_ip=''
   resolved_ip=$(nslookup "*.$DNS_DOMAIN" | grep Address | grep -v ':53' | cut -d ' ' -f2)
-  return [ "$resolved_ip" != "$external_static_ip" ]
+  return test [ "$resolved_ip" != "$external_static_ip" ]
 }
 
 with_backoff check_resolved_ip
